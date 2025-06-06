f092f88db784196629de37f3773424982886b338
saruulgerel <saruulgerel.o325@gmail.com>
2025-06-06 10:36:09 +0800

    dn

---
+++ src/preload.js
@@ -0,0 +1,7 @@
+// const { contextBridge } = require("electron");
+// const remote = require("@electron/remote");
+
+// contextBridge.exposeInMainWorld("electronAPI", {
+//   Menu: remote.Menu,
+//   desktopCapturer: require("electron").desktopCapturer,
+// });
